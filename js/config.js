02900ed095607685fcce1feee3f7dc05dcf999c0
derek3762 <derek3762@gmail.com>
2024-12-25 16:44:26 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_50gX49Q0l', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5f3kbbssgnmr2trq5qsprjf2pa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',

7d1f889ceb22e34f63b0ed9816dad5138ce63899
derek3762 <derek3762@gmail.com>
2025-01-18 12:03:42 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_dWQCB84Li', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6123h8nvs2uho02n14hubnccaj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_j7AoJ9S6c', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1rds59cbi3i9keen26hjfe90f3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {

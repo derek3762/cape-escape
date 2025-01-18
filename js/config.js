a8a6835d45eef0500a6bc4ef4300fcd1bba92d41
derek3762 <derek3762@gmail.com>
2025-01-18 11:43:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_50gX49Q0l', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5f3kbbssgnmr2trq5qsprjf2pa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_2yLAjYzU6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'lmr4focnkfmt5ngoli73b5imf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
